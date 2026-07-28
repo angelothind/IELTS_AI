68b255da868ba9a528803efb6ebd464859119d8d
angelothind <angelo.thind@gmail.com>
2026-07-28 16:53:42 +0700

    Basic pages object + alignment of toolbar and page added and modded in app.css

--- ieltsai-frontend/src/App.jsx
+++ ieltsai-frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Toolbar from './toolbar';
+import Pages from './pages';
 
 function App() {
   return <main className="app">
     <Toolbar />
+    <Pages />
   </main>
 }
 
---
+++ ieltsai-frontend/src/pages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './pages.css';
+
+const Pages = () => {
+    return (
+        <div className="pages">
+            
+        </div>
+    )
+}
+
+export default Pages;
