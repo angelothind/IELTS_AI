49f0d4cf0a5dce93d551c6c1ef8a598dd6b9876c
angelothind <angelo.thind@gmail.com>
2026-08-19 16:40:45 +0700

    basic Fast API setup

---
+++ ieltsai-backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from app.routers.health import router as health_route
+
+app = FastAPI()
+
+app.include_router(health_route)
+
+@app.get("/")
+def read_root():
+    return {"message": "IELTS backend is running"}
+
+    
